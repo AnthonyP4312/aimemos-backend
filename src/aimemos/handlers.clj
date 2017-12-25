9ea737ba7e4613e797f2dafbf60bf15949933bd9
Anthony Palmer <anthony@motivislearning.com>
2017-12-25 17:35:15 -0600

    Refactored into several files

--- src/aimemos/handlers.clj
+++ src/aimemos/handlers.clj
@@ -1,1 +1,71 @@
-(ns aimemos.handlers)
+(ns aimemos.handlers
+  (:require [aimemos.db :refer :all]
+            [aimemos.user-api :as api]
+            [cheshire.core :as json]
+            [buddy.auth :refer [authenticated?]]
+            [org.httpkit.server :refer :all]
+            [buddy.hashers :refer [derive check] :rename {derive bcrypt}]))
+
+(defn socket-handler
+  "Receives a JSON string and performs various operations based on the contents"
+  ;; :method - message or api call
+  ;; :params - map of relevant parameters to method
+  [user, m]
+  (println "oh shit socket action going on")
+  (println m)
+  (println (json/decode m true))
+  (let [{:keys [method params]} (json/decode m true)]
+    (case method
+      "send-message" (api/send-message (assoc params :from user))
+      "add-buddy" (api/add-buddy (assoc params :username user))
+      "delete-buddy" (api/delete-buddy (assoc params :username user))
+      "update-status" (api/update-status (assoc params :username user))
+      "update-groupname" (api/update-groupname (assoc params :username user))
+      "buddies-by-user" (api/buddies-by-user {:username user}))))
+
+
+(defn disconnect!
+  [user status]
+  (println (str "shits dead my dude: " user status))
+  (swap! current-users dissoc (keyword user))
+  (println "New CUrrent Users: " @current-users))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Route Handlers
+
+(defn chat-handler [req]
+
+  (println req)
+  (println "WEBSOCKET REQUEST =====")
+  (if (authenticated? req)
+    (let [res (with-channel req channel
+                (on-close channel (partial disconnect! (:identity req))) 
+                (on-receive channel (partial socket-handler (:identity req))))]
+      (println "oh")
+      (swap! current-users assoc (keyword (:identity req)) (:body res))
+      res)
+    {:status 401}))
+
+
+
+(defn create-account [user pass]
+  (println user)
+  (println pass)
+  (add-user db {:username user
+                :password (bcrypt pass)
+                :status "OFFLINE"}))
+
+
+(defn buddy-list [req]
+  "Returns a map of buddies by user"
+  (println (str req))
+  (let [buddies (buddies-by-user db {:username req})]
+    (println buddies "\n\n")
+
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+     :body (json/encode buddies)}))
+  
+
--- src/aimemos/routes.clj
+++ src/aimemos/routes.clj
@@ -1,1 +1,37 @@
-(ns aimemos.routes)
+(ns aimemos.routes
+  (:require [buddy.auth.middleware :refer [wrap-authorization
+                                           wrap-authentication]]
+            [buddy.auth.backends :refer [basic token]]
+            [buddy.hashers :refer [derive check] :rename {derive bcrypt}]
+            [compojure.core :refer :all]
+            [aimemos.db :refer :all]
+            [aimemos.handlers :refer :all]))
+
+(defn my-authfn [req auth]
+  (println auth)
+  (when (check (:password auth) 
+               (:password (password-by-user db {:username (:username auth)})))
+    (:username auth)))
+
+(defroutes webpage
+  (GET  "/aim" req nil)
+  (GET  "/aim/signup" req nil)
+  (POST "/aim/signup" [user pass] (create-account user pass)))
+
+(defroutes user-login
+  (GET  "/ws" req (chat-handler req)))
+
+(defroutes user-api
+  (POST "/aim/add-buddy" req nil)
+  (POST "/aim/delete-buddy" req nil)
+  (GET "/aim/buddies-by-user/:user" [user] (buddy-list user))
+  (POST "/aim/update-status" req nil)
+  (POST "/aim/update-groupname" req nil))
+
+(def app 
+  (routes
+   user-api
+   webpage
+   (-> user-login
+       (wrap-authentication (basic {:authfn my-authfn}))
+       (wrap-authorization (basic {:authfn my-authfn})))))
