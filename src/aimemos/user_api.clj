d4cbc54b4a85b1f85b13f03980379fd3ec8da4aa
Anthony Palmer <anthony@motivislearning.com>
2017-12-28 17:09:21 -0600

    added json encoding for user-api methods

--- src/aimemos/user_api.clj
+++ src/aimemos/user_api.clj
@@ -28,13 +28,15 @@
                                    "Sorry. Have an error instead.\n\n"
                                    (.getMessage ex))})))))
 
+
+
 (defn add-buddy 
   "{:username :buddyname :groupname}\n  Adds adds one user to another's
   buddylist under the groupname provided"
   [params]
   (when (safe-db (db/add-buddy db params))
     (send! (@current-users (keyword (:username params))) 
-           (db/buddies-by-user db {:username (:username params)}))))
+           (json/encode (db/buddies-by-user db {:username (:username params)})))))
 
 
 (defn delete-buddy
@@ -42,7 +44,7 @@
   [params]
   (when (safe-db (db/delete-buddy db params))
     (send! (@current-users (keyword (params :username)))
-           (db/buddies-by-user db {:username (params :username)}))))
+           (json/encode (db/buddies-by-user db {:username (params :username)})))))
 
 (defn update-status
   "{:username :status}\n  Toggle the user between the statuses of ONLINE,
@@ -54,7 +56,7 @@
     (let [users (db/users-by-buddy {:buddyname (params :username)})]
       (doseq [user users]
         (when-let [user-chan (@current-users (keyword user))]
-          (send! user-chan (db/buddies-by-user db {:username user})))))))
+          (json/encode (send! user-chan (db/buddies-by-user db {:username user}))))))))
 
 (defn update-groupname
   "{:username :buddyname :groupname}\n  Change the group specified for a
@@ -62,10 +64,10 @@
   [params]
   (when (safe-db (db/update-groupname db params))
     (send! (@current-users (keyword (params :username)))
-           (db/buddies-by-user db {:username (params :username)}))))
+           (json/encode (db/buddies-by-user db {:username (params :username)})))))
 
 (defn buddies-by-user
   "{:username}\n  Return the current list of buddies for a user"
   [params]
   (send! (@current-users (keyword (params :username))) 
-         (db/buddies-by-user db params)))
+         (json/encode (db/buddies-by-user db params))))
